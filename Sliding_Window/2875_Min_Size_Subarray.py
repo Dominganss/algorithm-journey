6e0a8c711c5d397446364dc0be27a30e1b852110
Dominganss <1216689979@qq.com>
2026-05-16 14:53:19 +0800

    Add thoughts on infinite loop array concept
    Add thoughts section explaining the infinite loop array concept.

--- Sliding_Window/2875_Min_Size_Subarray.py
+++ Sliding_Window/2875_Min_Size_Subarray.py
@@ -17,3 +17,6 @@ class Solution:
         return -1 if ans == inf else ans + k * n #未进入循环直接输出初始答案，因此可以在对变量进行初始赋值时，用一个答案永远不会出现的值。
             
             
+"""@THOUGHTS
+在这个通过数组nums所形成的无限循环的数组中，任意长度为n（ n == len(nums)）的子数组，其中的元素及其各元素的个数都与nums相同
+"""
