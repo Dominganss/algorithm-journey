637d0d1433ebf461389eb34027d3846585c35701
Dominganss <1216689979@qq.com>
2026-05-16 13:56:20 +0800

    Optimize minSizeSubarray function and add comments
    Optimize memory usage by adding a comment on the new_nums creation and clarify the initial answer assignment.

--- Sliding_Window/2875_Min_Size_Subarray.py
+++ Sliding_Window/2875_Min_Size_Subarray.py
@@ -1,6 +1,6 @@
 class Solution:
     def minSizeSubarray(self, nums: List[int], target: int) -> int:
-        new_nums = nums * 2
+        new_nums = nums * 2 #优化 可以通过求模运算来节省内存。
         total = sum(nums)
         new_target = target % total
         k = target // total
@@ -14,6 +14,6 @@ class Solution:
                 left += 1
             if s == new_target:
                 ans = min(ans,i - left + 1)
-        return -1 if ans == inf else ans + k * n
+        return -1 if ans == inf else ans + k * n #未进入循环直接输出初始答案，因此可以在对变量进行初始赋值时，用一个答案永远不会出现的值。
             
             
